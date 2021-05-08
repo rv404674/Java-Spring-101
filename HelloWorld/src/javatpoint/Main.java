6f6ad727d74b4093769d2411ff77d6148ac5ca13
rv404674 <rv404674@gmail.com>
2021-05-08 16:14:50 +0530

    Added a gitignore

--- HelloWorld/src/javatpoint/Main.java
+++ HelloWorld/src/javatpoint/Main.java
@@ -1,16 +1,30 @@
 package javatpoint;
 
+import basics.Account;
+import basics.VipCustomer;
+
 public class Main {
 
-    public static void main(String[] args){
-        Car porche = new Car();
-        porche.numberplate = 1234;
+    public static void main(String[] args) {
+        Account account = new Account();
+        // Account account = new Account("12345xyz", 10000, "rv404674@gmail.com", "Rahul Verma", "7018471136");
 
-        System.out.println(porche.getModel());
-        porche.setModel("Safari 1200");
-        System.out.println(porche.getModel());
+        // NOTE: This is a tedious thing to do, if you need to do it for every customer.
+        // use constructor
+//        account.setCustomerName("Rahul Verma");
+//        account.setCustomerEmailAddress("rv404674@gmail.com");
+//        account.withDrawal(100);
+//        System.out.println(account.getCustomerEmailAddress());
+//        account.withDrawal(1000);
+//
+//        System.out.println(account.getBalance());
+//
+//        Account timAccount  = new Account("tim", "tim@gmail.com", "1999");
 
-        porche.setDoor("Front Door");
-        System.out.println(porche.getDoor());
+        VipCustomer vipCustomer1 = new VipCustomer();
+        System.out.println(vipCustomer1.getEmailAddress());
+
+        VipCustomer vipCustomer2 = new VipCustomer(5000, "rv404674@gmail.com");
+        System.out.println(vipCustomer1.getCreditLimit());
     }
 }
