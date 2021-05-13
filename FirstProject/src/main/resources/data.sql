689fb5a3116cfcd3659560463de5e73105a6b183
rv404674 <rv404674@gmail.com>
2021-05-13 23:41:14 +0530

    Added some new changes to pom.xml

---
+++ FirstProject/src/main/resources/data.sql
@@ -0,0 +1,5 @@
+insert into alien values(101, 'Navin', 'HR');
+insert into alien values(102, 'Rahul', 'Tech');
+insert into alien values(103, 'Sachin', 'QA');
+insert into alien values(104, 'Sumedha', 'Tech');
+insert into alien values(105, 'Shubham', 'Tech');
