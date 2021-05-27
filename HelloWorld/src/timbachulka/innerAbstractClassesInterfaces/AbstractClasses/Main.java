8c0fbeda52988aa87acb6f041eae84e9a43fe96e
Rahul Verma <rv404674@gmail.com>
2021-05-27 19:54:20 +0530

    Abstract classes basics

---
+++ HelloWorld/src/timbachulka/innerAbstractClassesInterfaces/AbstractClasses/Main.java
@@ -0,0 +1,9 @@
+package timbachulka.innerAbstractClassesInterfaces.AbstractClasses;
+
+public class Main {
+    public static void main(String[] args){
+        Dog dog = new Dog("rubu");
+        dog.eat();
+        dog.breathe();
+    }
+}
